fb5d37ae165b690a4064da3c2bdf252e9fd2eda6
smile <smayluzb@gmail.com>
2022-10-29 17:23:48 +0500

    cle

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+var name = 'Davom ettirish uchun OK ni bosing'
+
+alert(name)
+
+
+var userName = prompt('Ismingizni kiriting')
+alert('salom senga '+ userName)
+
+
+var userYear = +prompt('Yilingiz ?')
+var hozir = (2022)
+alert('Sizning ismingiz ' + userName + ' .Sizning yoshingiz ' + (hozir - userYear) + 'da' )
+
+
+alert('Hisoblang ! ')
+
+
+var exercise = +prompt  ('34+63') 
+var exercise = 97
+
+alert('Togri Javob = ' + exercise + ' Javobingiz ' +  exercise )
+
+
+
